e4bf618651959ed239f1c4c96ccb99e60dc7d375
Trevor Litsey <trevorlitsey@gmail.com>
2019-08-04 18:25:59 -0500

    add tomato ficon

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `🍅`, // This path is relative to the root of the site.
+        icon: `src/images/tomato.jpeg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
